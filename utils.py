5eab7daf7c69f1d9047e196a2ab99403deb21955
Haimeng Zhao <haimengzhao@icloud.com>
2021-09-20 16:51:39 +0800

    clean utils

--- utils.py
+++ utils.py
@@ -1,12 +1,5 @@
-import pandas as pd
-from torch import nn
 import numpy as np
 import h5py
-import matplotlib.pyplot as plt
-import torch
-import joblib
-from sklearn import preprocessing
-from torch.utils.data import Dataset
 import pywt
 
 # 小波滤噪
@@ -25,13 +18,13 @@ def wavelet_denoising(data):
 
 def lossfunc_eval(y, data):
     t = data.get_label()
-    loss = (y-t)**2/t
+    loss = (y - t) ** 2 / t
     return "custom", np.mean(loss), False
 
 def lossfunc_train(y, data):
     t = data.get_label()
-    grad = 2*(y-t)/t
-    hess = 2*np.ones_like(y)/t
+    grad = 2 * (y - t) / t
+    hess = 2 * np.ones_like(y) / t
     return grad, hess
 
 def tkeo(arr):
@@ -63,14 +56,15 @@ def loadData(datapath, datatype='geo'):
         
         # 读取训练需要的数据
         if datatype=='pro':
-            X = torch.Tensor(np.array(file['X']))
-            Y = torch.Tensor(np.array(file['Y']))
+            X = np.array(in_file['X'])
+            Y = np.array(in_file['Y'])
             return X, Y
         
         # PET = np.array(np.array(file['PETruth']).tolist())
         # 如果是另外的datatype，则返回Waveform
         WF = in_file['Waveform'][...]
         return WF
+
 def getNum(dataset):
     '''
     getNum: 返回每个EventID对应的PE数或波形数。
@@ -135,72 +129,12 @@ def getCancel(maxIndex, maxValue):
     absArray = maxValue - np.abs(np.arange(1000) - maxIndex) * step
     return np.where(absArray > 0, absArray, 0)
 
-class TrainData(Dataset):
-    def __init__(self, folder_path):
-        self.X, self.Y = loadData(folder_path, 'pro')
-
-    def __getitem__(self, index):
-        return {'X':self.X[index], 'Y':self.Y[index]}
-
-    def __len__(self):
-        return len(self.Y)
-
 def saveData(X, Y, path):
     h5 = h5py.File(path,'w')
     dataX = h5.create_dataset(name='X', data=X)
     dataY = h5.create_dataset(name='Y', data=Y)
     h5.close()
 
-class Bottleneck(nn.Module):
-    def __init__(self, planes):
-        super(Bottleneck, self).__init__()
-        self.conv1 = nn.Conv1d(planes, planes, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm1d(planes)
-        self.conv2 = nn.Conv1d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm1d(planes)
-        self.conv3 = nn.Conv1d(planes, planes, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm1d(planes)
-        self.relu1 = nn.PReLU()
-        self.relu2 = nn.PReLU()
-        self.relu3 = nn.PReLU()
-
-    def forward(self, x):
-        residual = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu1(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.relu2(out)
-
-        out = self.conv3(out)
-        out = self.bn3(out)
-
-        out += residual
-        out = self.relu3(out)
-
-        return out
-
-def res_layers(planes, num_blocks, ):
-    return nn.Sequential(*[Bottleneck(planes) for i in range(num_blocks)])
-
-def conv_downsample(in_planes, out_planes):
-    return nn.Sequential(
-        nn.ReflectionPad1d(2),
-        nn.Conv1d(in_planes, out_planes, kernel_size=5, stride=2, bias=False),
-        nn.BatchNorm1d(out_planes),
-        nn.PReLU(),
-    )
-
-def conv_same(in_planes, out_planes):
-    return nn.Sequential(
-        nn.ReflectionPad1d(1),
-        nn.Conv1d(in_planes, out_planes, kernel_size=3, stride=1, bias=False),
-        nn.BatchNorm1d(out_planes),
-        nn.PReLU(),
-    )
 
 def saveans(ans, path):
     h5 = h5py.File(path,'w')
