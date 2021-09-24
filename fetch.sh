7b4f3f42aaa6a9f80d6e7466788f02d1f6fe50cc
mhliu0001 <mhliu0001@sina.com>
2021-09-24 21:06:39 +0800

    给脚本加入适当注释

--- fetch.sh
+++ fetch.sh
@@ -4,13 +4,16 @@
 
 fileArray=(geo.h5 final.h5)
 
+# 更新fileArray，加入final-*.h5
 for num in $(seq 2 19)
 do
 	fileArray[$num]=final-${num}.h5
 done
 
+# 下载数据
 for file in ${fileArray[@]}
 do
+	# 当目录中没有这个文件才下载
 	if ! test -f ./data/${file}
 	then
 		wget -P ./data/ http://hep.tsinghua.edu.cn/~orv/dc/bdeph2021/${file}
--- prompt.sh
+++ prompt.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# 此脚本用于设置答案的文件名序号和log
+# 答案的文件名格式为ans${num}.h5，其中num是正整数
+# log将被写入到./ans/log中
+
 echo "请输入答案的文件名序号:"
 read num
 
+# 判断num是否合法，以及文件是否已存在
 while echo $num | grep -q [^0-9] || [[ $num -le 0 ]] || [[ -e ./ans/ans${num}.h5 ]]
 do
 	if [[ -e ./ans/ans${num}.h5 ]]
@@ -17,5 +22,8 @@ done
 echo "请输入本次运行的log:"
 read log
 
+# 修改final.ipynb中的输出路径
 sed -E "s/ans[0-9]+\.h5/ans${num}.h5/g" -i ./final.ipynb
-echo "ans${num}.h5: $log"
+
+# 将log输出到./ans/log
+echo "ans${num}.h5: $log" >> ./ans/log
